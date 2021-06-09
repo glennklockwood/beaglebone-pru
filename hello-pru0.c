4d70eb503a526f3bc5c2353c935fbab54e2c70a0
Glenn K. Lockwood <glennklockwood@gmail.com>
2021-06-08 22:08:00 -0700

    strip more out

--- hello-pru0.c
+++ hello-pru0.c
@@ -3,86 +3,45 @@
 #include <rsc_types.h>
 #include <pru_cfg.h>
 
-// These are addresses for the am35xx
-#define GPIO0 0x44E07000        // From table 2.2 of am335x TRM
-#define GPIO1 0x4804C000
-#define GPIO2 0x481AC000
-#define GPIO3 0x481AE000
+#define GPIO1 0x4804C000    /* am335x GPIO address */
 
-// USR LED bit positions
-// GPIO1
-#define USR0 (1<<21)
-#define USR1 (1<<22)
-#define USR2 (1<<23)
-#define USR3 (1<<24)
+/* USR LED bit positions */
+#define USR0 (1 << 21)
+#define USR1 (1 << 22)
+#define USR2 (1 << 23)
+#define USR3 (1 << 24)
 
-// The define a couple of GPIO pin addresses on Black
-// GPIO1
-#define P9_14 (1<<18)
-#define P9_16 (1<<19)
-
-// R30 output bits on pru0
-#define P9_31   (1<<0)
-#define P9_29   (1<<1)
-#define P9_30   (1<<2)
-#define P9_28   (1<<3)
-#define P9_92   (1<<4)
-#define P9_27   (1<<5)
-#define P9_91   (1<<6)
-#define P9_25   (1<<7)
-
-// Shared memory
-#define AM33XX_DATARAM0_PHYS_BASE		0x4a300000
-#define AM33XX_DATARAM1_PHYS_BASE		0x4a302000
-#define AM33XX_PRUSS_SHAREDRAM_BASE		0x4a310000
-
-// /4 to convert from byte address to word address
-#define GPIO_CLEARDATAOUT	0x190/4     // Write 1 here to set a given bit    
-#define GPIO_SETDATAOUT 	0x194/4     // A 1 here clears the corresponding bit
-#define GPIO_DATAOUT		0x138/4     // For reading the GPIO registers
+/* /4 to convert from byte address to word address */
+#define GPIO_LOW    (0x190 / 4)
+#define GPIO_HIGH   (0x194 / 4)
 
 struct my_resource_table {
-	struct resource_table base;
-
-	uint32_t offset[1]; /* Should match 'num' in actual definition */
+    struct resource_table base;
+    uint32_t offset[1];
 };
 
 volatile register unsigned int __R30;
 volatile register unsigned int __R31;
 
 void main(void) {
-	int i;
+    uint32_t *gpio1 = (uint32_t *)GPIO1;
+    
+    /* Clear SYSCFG[STANDBY_INIT] to enable OCP master port */
+    CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
 
-	uint32_t *gpio1 = (uint32_t *)GPIO1;
-	
-	/* Clear SYSCFG[STANDBY_INIT] to enable OCP master port */
-	CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
-
-	for(i=0; i<10; i++) {
-		gpio1[GPIO_SETDATAOUT]   = USR3;	// The the USR3 LED on
-
-		__delay_cycles(500000000/5);    	// Wait 1/2 second
-
-		gpio1[GPIO_CLEARDATAOUT] = USR3;
-
-		__delay_cycles(500000000/5); 
-
-	}
-	__halt();
+    while (1) {
+        gpio1[GPIO_HIGH] = USR3;    // The the USR3 LED on
+        __delay_cycles(500000000/5);        // Wait 1/2 second
+        gpio1[GPIO_LOW] = USR3;
+        __delay_cycles(500000000/5); 
+    }
 }
 
 #pragma DATA_SECTION(pru_remoteproc_ResourceTable, ".resource_table")
 #pragma RETAIN(pru_remoteproc_ResourceTable)
 struct my_resource_table pru_remoteproc_ResourceTable = {
-	1,	/* we're the first version that implements this */
-	0,	/* number of entries in the table */
-	0, 0,	/* reserved, must be zero */
-	0,	/* offset[0] */
+    1,      /* we're the first version that implements this */
+    0,      /* number of entries in the table */
+    0, 0,   /* reserved, must be zero */
+    0,      /* offset[0] */
 };
-
-
-// Turns off triggers
-#pragma DATA_SECTION(init_pins, ".init_pins")
-#pragma RETAIN(init_pins)
-const char init_pins[] =  
-	"/sys/class/leds/beaglebone:green:usr3/trigger\0none\0\0\0";
