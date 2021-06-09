38bc68b456f128e648fb4ac666c06ea575c7f83d
Glenn K. Lockwood <glennklockwood@gmail.com>
2021-06-08 22:19:24 -0700

    further strip down example

--- hello-pru0.c
+++ hello-pru0.c
@@ -1,39 +1,29 @@
 #include <stdint.h>
-#include <stddef.h>
-#include <rsc_types.h>
-#include <pru_cfg.h>
+#include <rsc_types.h>  /* provides struct resource_table */
 
-#define GPIO1 0x4804C000    /* am335x GPIO address */
+#define GPIO1 0x4804C000        /* am335x GPIO address */
 
-/* USR LED bit positions */
-#define USR0 (1 << 21)
-#define USR1 (1 << 22)
-#define USR2 (1 << 23)
-#define USR3 (1 << 24)
+#define LED_USR3 (1 << 24)      /* USR LED offset */
 
 /* /4 to convert from byte address to word address */
 #define GPIO_LOW    (0x190 / 4)
 #define GPIO_HIGH   (0x194 / 4)
+
+#define CYCLES_PER_SECOND 200000000 /* PRU has 200 MHz clock */
 
 struct my_resource_table {
     struct resource_table base;
     uint32_t offset[1];
 };
 
-volatile register unsigned int __R30;
-volatile register unsigned int __R31;
-
 void main(void) {
     uint32_t *gpio1 = (uint32_t *)GPIO1;
     
-    /* Clear SYSCFG[STANDBY_INIT] to enable OCP master port */
-    CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
-
     while (1) {
-        gpio1[GPIO_HIGH] = USR3;    // The the USR3 LED on
-        __delay_cycles(500000000/5);        // Wait 1/2 second
-        gpio1[GPIO_LOW] = USR3;
-        __delay_cycles(500000000/5); 
+        gpio1[GPIO_HIGH] = LED_USR3;
+        __delay_cycles(CYCLES_PER_SECOND / 2); /* wait 0.5 seconds */
+        gpio1[GPIO_LOW] = LED_USR3;
+        __delay_cycles(CYCLES_PER_SECOND / 2); /* wait 0.5 seconds */
     }
 }
 
