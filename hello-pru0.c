32e16785ef4cd656cd0d6c85c1e5780d41bf8299
Glenn K. Lockwood <glennklockwood@gmail.com>
2021-06-12 16:08:16 -0700

    switch to using native pru register

--- hello-pru0.c
+++ hello-pru0.c
@@ -1,36 +1,24 @@
 #include <stdint.h>
 #include <rsc_types.h>  /* provides struct resource_table */
 
-#define GPIO1 0x4804C000        /* am335x GPIO address */
-
-#define LED_USR3 (1 << 24)      /* USR LED offset */
-
-/* /4 to convert from byte address to word address */
-#define GPIO_LOW    (0x190 / 4)
-#define GPIO_HIGH   (0x194 / 4)
-
 #define CYCLES_PER_SECOND 200000000 /* PRU has 200 MHz clock */
 
+volatile register uint32_t __R30; /* output register for PRU */
+
+/* R30 at 0x1 = pru1_pru0_pru_r30_0 = pin 31 = ball A13 = P9_31 */
+#define P9_31 (1 << 0)
 void main(void) {
-    uint32_t *gpio1 = (uint32_t *)GPIO1;
-    
     while (1) {
-        gpio1[GPIO_HIGH] = LED_USR3;
-        __delay_cycles(CYCLES_PER_SECOND / 2); /* wait 0.5 seconds */
-        gpio1[GPIO_LOW] = LED_USR3;
-        __delay_cycles(CYCLES_PER_SECOND / 2); /* wait 0.5 seconds */
+        __R30 |= P9_31; /* set first bit of register 30 to 1 */
+        __delay_cycles(CYCLES_PER_SECOND / 4); /* wait 0.5 seconds */
+        __R30 &= ~P9_31; /* set first bit of register 30 to 0 */
+        __delay_cycles(CYCLES_PER_SECOND / 4); /* wait 0.5 seconds */
     }
 }
 
-/* without the following resource table,starting the PRU will generate these
- * errors in syslog:
- *
- * kernel: remoteproc remoteproc1: header-less resource table
- * kernel: remoteproc remoteproc1: Boot failed: -22
- */
 #pragma DATA_SECTION(pru_remoteproc_ResourceTable, ".resource_table")
 #pragma RETAIN(pru_remoteproc_ResourceTable)
 struct my_resource_table {
     struct resource_table base;
     uint32_t offset[1];
-} pru_remoteproc_ResourceTable = { 1, 0, 0, 0, 0 };;
+} pru_remoteproc_ResourceTable = { 1, 0, 0, 0, 0 };
