16169dd67b4d1d96a23281b99537c897cbf3376d
Glenn K. Lockwood <glennklockwood@gmail.com>
2021-06-12 16:10:51 -0700

    even more simple

--- hello-pru0.c
+++ hello-pru0.c
@@ -3,19 +3,22 @@
 
 #define CYCLES_PER_SECOND 200000000 /* PRU has 200 MHz clock */
 
+#define P9_31 (1 << 0) /* R30 at 0x1 = pru1_pru0_pru_r30_0 = ball A13 = P9_31 */
+#define SET_BIT(reg, bit) (reg) |= (bit)
+#define REMOVE_BIT(reg, bit) (reg) &= ~(bit)
+
 volatile register uint32_t __R30; /* output register for PRU */
 
-/* R30 at 0x1 = pru1_pru0_pru_r30_0 = pin 31 = ball A13 = P9_31 */
-#define P9_31 (1 << 0)
 void main(void) {
     while (1) {
-        __R30 |= P9_31; /* set first bit of register 30 to 1 */
+        SET_BIT(__R30, P9_31);
         __delay_cycles(CYCLES_PER_SECOND / 4); /* wait 0.5 seconds */
-        __R30 &= ~P9_31; /* set first bit of register 30 to 0 */
+        REMOVE_BIT(__R30, P9_31);
         __delay_cycles(CYCLES_PER_SECOND / 4); /* wait 0.5 seconds */
     }
 }
 
+/* required by PRU */
 #pragma DATA_SECTION(pru_remoteproc_ResourceTable, ".resource_table")
 #pragma RETAIN(pru_remoteproc_ResourceTable)
 struct my_resource_table {
